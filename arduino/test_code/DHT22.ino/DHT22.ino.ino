b529516860fac6612faa19993c22f2909d3a9f32
jebuss <jens.buss@udo.edu>
2016-07-14 13:42:39 +0200

    a test script for the DHT22 Sensor

---
+++ arduino/test_code/DHT22.ino/DHT22.ino.ino
@@ -0,0 +1,35 @@
+#include "DHT.h"
+ 
+#define DHTPIN 9     
+#define DHTTYPE DHT22 //DHT11, DHT21, DHT22
+ 
+DHT dht(DHTPIN, DHTTYPE);
+ 
+void setup() 
+{
+  Serial.begin(9600); 
+  Serial.println("DHT22 - Test!");
+ 
+  dht.begin();
+}
+ 
+void loop() 
+{
+  float h = dht.readHumidity();     //Luftfeuchte auslesen
+  float t = dht.readTemperature();  //Temperatur auslesen
+ 
+  // Prüfen ob eine gültige Zahl zurückgegeben wird. Wenn NaN (not a number) zurückgegeben wird, dann Fehler ausgeben.
+  if (isnan(t) || isnan(h)) 
+  {
+    Serial.println("DHT22 konnte nicht ausgelesen werden");
+  } 
+  else
+  {
+    Serial.print("Luftfeuchte: "); 
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperatur: "); 
+    Serial.print(t);
+    Serial.println(" C");
+  }
+}
