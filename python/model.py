0bec64a9250647f98248d1f7f07639f996f79894
Jens Buss <jens.buss@udo.edu>
2016-08-22 00:39:27 +0200

    allow null, make timestamp unique

--- python/model.py
+++ python/model.py
@@ -4,23 +4,23 @@ db = MySQLDatabase("factdata")
 
 class FactWeather(Model):
     cycles =  IntegerField()
-    humidity0 = FloatField()
-    humidity1 = FloatField()
+    humidity0 = FloatField(null = True)
+    humidity1 = FloatField(null = True)
 
-    m152_counter = IntegerField()
-    m152_state = BooleanField()
-    rg1_drop_count = IntegerField()
-    rg1_total_rainfall = FloatField()
-    rg1_mean_drop_size = FloatField()
-    rg2_tip_count = IntegerField()
-    rg2_total_rainfall = FloatField()
-    temperature0 = FloatField()
-    temperature1 = FloatField()
+    m152_counter = IntegerField(null = True)
+    m152_state = BooleanField(null = True)
+    rg1_drop_count = IntegerField(null = True)
+    rg1_total_rainfall = FloatField(null = True)
+    rg1_mean_drop_size = FloatField(null = True)
+    rg2_tip_count = IntegerField(null = True)
+    rg2_total_rainfall = FloatField(null = True)
+    temperature0 = FloatField(null = True)
+    temperature1 = FloatField(null = True)
 
-    board_time = IntegerField()
-    timestamp = DateTimeField()
-    wind_direction = FloatField()
-    wind_speed = FloatField()
+    board_time = IntegerField(null = True)
+    timestamp = DateTimeField(unique = True, index = True)
+    wind_direction = FloatField(null = True)
+    wind_speed = FloatField(null = True)
 
     class Meta:
         database = db # This model uses the "people.db" database.
