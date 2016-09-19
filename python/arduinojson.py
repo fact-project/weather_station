20609fce6e4e4f2c2fc3b4e106a9fd251be1bead
Jens Buss <jens.buss@udo.edu>
2016-09-19 12:40:26 +0200

    write json

--- python/arduinojson.py
+++ python/arduinojson.py
@@ -5,6 +5,7 @@ import serial
 import json
 import datetime
 import numpy as np
+import pandas as pd
 from pprint import PrettyPrinter
 
 pp = PrettyPrinter()
@@ -14,21 +15,25 @@ def main():
     ser = serial.Serial("/dev/ttyACM0",9600,timeout=1)
 
     pp.pprint("Startup")
-    while (True):
-        if (ser.inWaiting()>0):
-            """Get a line of data, strip out the null characters and new lines"""
-            data = str(ser.readline()).replace("b\'","").replace("\\r\\n\'","")
-            try:
-                d = json.loads(data)
-                d["time_stamp"] = datetime.datetime.utcnow()
-			
-                pp.pprint(d)
-		print "\n"
-                count = count + 1
-            except (KeyboardInterrupt, SystemExit):
-                raise
-            except:
-                print("Data not readable")
+    with open('/home/pi/mnt/weather_data/data.json', 'a') as outfile:
+        while True:
+            if ser.inWaiting()>0:
+                """Get a line of data, strip out the null characters and new lines"""
+                data = str(ser.readline()).replace("b\'","").replace("\\r\\n\'","")
+                d = False
+                try:
+                    d = json.loads(data)
+                except (KeyboardInterrupt, SystemExit):
+                    raise
+                except:
+                    print("Data not readable")
+                if d:
+                    d["time_stamp"] = datetime.datetime.utcnow().isoformat()
+                    d["windDirectionDeg"] = convertVoltage2WindDirection(d["windDirection"]/1000.0)
+                    pp.pprint(d)
+                    count = count + 1
+                    json.dump(d, outfile)
+                    outfile.write('\n')
 
 if __name__ == "__main__":
 	main()
