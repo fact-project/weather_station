7f2f6db6a2d95c390b2e8db4f7be0f27648d33eb
Dominik Neise <neised@phys.ethz.ch>
2017-05-03 12:27:03 +0200

    pep8

--- yellow_box/example.py
+++ yellow_box/example.py
@@ -2,15 +2,18 @@ import socket
 import time
 import struct
 from collections import namedtuple
-Report = namedtuple('Report',
-    ['drop_counter_number_of_pulses',
-    'drop_counter_pulse_length',
-    'condensation_detector_number_of_pulses',
-    'condensation_detector_pulse_length',
-    'time_since_boot_in_ms',
-    'time_between_message_updates_in_ms',
+Report = namedtuple(
+    'Report',
+    [
+        'drop_counter_number_of_pulses',
+        'drop_counter_pulse_length',
+        'condensation_detector_number_of_pulses',
+        'condensation_detector_pulse_length',
+        'time_since_boot_in_ms',
+        'time_between_message_updates_in_ms',
     ]
 )
+
 
 def request(host, port=80):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,15 +26,16 @@ def request(host, port=80):
     s.close()
     return data
 
+
 if __name__ == "__main__":
     while True:
         try:
             msg = struct.unpack("IIIIIIH", request('10.0.100.155'))
             report = Report(
                 msg[0],
-                msg[1] / (msg[0]*1000) if msg[0] else '-',
+                msg[1] / (msg[0] * 1000) if msg[0] else '-',
                 msg[2],
-                msg[3] / (msg[2]*1000) if msg[2] else '-',
+                msg[3] / (msg[2] * 1000) if msg[2] else '-',
                 msg[4],
                 msg[5],
             )
